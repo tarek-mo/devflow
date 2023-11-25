175cbe9300a370a63dafe6d058ca9c075c64a96b
Tarek Mo <tarekttik@gmail.com>
2023-11-25 22:43:02 +0000

    ok

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -58,9 +58,7 @@ export async function deleteUser(params: DeleteUserParams) {
     // delete user from database
     // and questions, answers, comments, etc
     // get user question ids
-    const userQuestionIds = await Question.find({ author: user._id }).distinct(
-      "_id"
-    );
+    await Question.find({ author: user._id }).distinct("_id");
 
     await Question.deleteMany({ author: user._id });
 

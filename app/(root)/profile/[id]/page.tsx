a131146616ae45095aa43712b4ab0d8f66c1d87f
Tarek Mo <itistarek@gmail.com>
2023-12-24 10:47:06 +0000

    fixed profile image height

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -36,7 +36,7 @@ const Page = async ({ params, searchParams }: URLProps) => {
             alt="profile picture"
             width={140}
             height={140}
-            className="rounded-full object-cover"
+            className="h-[140px] w-[140px] rounded-full object-cover"
           />
           <div className="mt-3">
             <h2 className="h2-bold text-dark100_light900">
